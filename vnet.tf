3d134b4a33dcaee90a5d2052c0307fe8638ef14a
Krishna <devopswithkrishnareddy@gmail.com>
2026-02-17 08:52:39 +0530

    subnet block

--- vnet.tf
+++ vnet.tf
@@ -4,3 +4,12 @@ resource "azurerm_virtual_network" "network" {
     resource_group_name = azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
 }
+
+resource "azurerm_subnet" "subnet" {
+    name = "krishna-subnet"
+    address_prefixes = ["10.1.1.0/24"]
+    resource_group_name = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.network.name
+
+  
+}
